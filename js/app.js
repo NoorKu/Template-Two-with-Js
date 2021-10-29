ed71c2647baf0aeb38b3dd60e1ea19e9e39974a7
Noorku <noorskoulayb@gmail.com>
2021-10-29 12:00:52 -0700

    Creating The Project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let count = document.querySelector(".count");
+let progress = document.querySelector(".progress");
+let input = document.querySelector("input");
+let div = document.querySelector("div");
+let maxLength = input.getAttribute("maxLength");
+
+count.innerHTML = maxLength;
+input.oninput = function () {
+count.innerHTML = maxLength - this.value.length;
+count.innerHTML == 0 ? count.classList.add("zero") : count.classList.remove("zero");
+// Ser the progress
+progress.style.width = `${(this.value.length * 100) / maxLength}%`
+
+};
+
